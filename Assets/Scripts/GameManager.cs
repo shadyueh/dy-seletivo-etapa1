47e2b8e6b40401e5b2c03e1ef6671d59a0aa0f9e
Held Grijo <held.grijo@gmail.com>
2020-10-17 14:05:15 -0400

    Prototype menu

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GameManager : MonoBehaviour
+{
+    public void LoadLevel(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void CloseApp()
+    {
+        Application.Quit();
+    }
+}
